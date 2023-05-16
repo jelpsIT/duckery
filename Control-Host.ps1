b7b5af17158f4fafbcab62bc231518045f0f3c7a
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2023-05-16 15:45:42 +0100

    Update Control-Host.ps1

--- Control-Host.ps1
+++ Control-Host.ps1
@@ -2,6 +2,12 @@ function Show-Menu {
     Write-Host "1. Lock PC"
     Write-Host "2. Custom Function"
     Write-Host "3. Test Function"
+    Write-Host "4. Exit"
+}
+
+function Select-IPAddress {
+    $ipAddress = Read-Host "Enter the client's IP address: "
+    return $ipAddress
 }
 
 function Send-CodeToClient {
@@ -31,39 +37,43 @@ function Send-CodeToClient {
         }
     }
 
-    Show-Menu
-    $selection = Read-Host "Select a function (1-3): "
+    while ($true) {
+        Show-Menu
+        $selection = Read-Host "Select a function (1-4): "
 
-    # Validate the selection
-    if ($selection -lt 1 -or $selection -gt 3) {
-        Write-Host "Invalid selection."
-        return
+        # Validate the selection
+        if ($selection -eq 4) {
+            break  # Exit the loop and return to IP selection screen
+        }
+        elseif ($selection -lt 1 -or $selection -gt 3) {
+            Write-Host "Invalid selection."
+            continue  # Continue to next iteration of the loop
+        }
+
+        # Get the function name based on the selection
+        $functionName = @{
+            1 = "LockPC"
+            2 = "Custom"
+            3 = "Test"
+        }[[int]$selection]  # Cast $selection as an integer
+
+        # Select the appropriate function based on the provided FunctionName
+        if ($functions.ContainsKey($functionName)) {
+            $code = $functions[$functionName].ToString()
+        }
+        else {
+            Write-Host "Invalid FunctionName."
+            continue  # Continue to next iteration of the loop
+        }
+
+        $writer.WriteLine($code)
+        $writer.Flush()
     }
-
-    # Get the function name based on the selection
-    $functionName = @{
-        1 = "LockPC"
-        2 = "Custom"
-        3 = "Test"
-    }[[int]$selection]  # Cast $selection as an integer
-
-    # Select the appropriate function based on the provided FunctionName
-    if ($functions.ContainsKey($functionName)) {
-        $code = $functions[$functionName].ToString()
-    }
-    else {
-        Write-Host "Invalid FunctionName."
-        return
-    }
-
-    $writer.WriteLine($code)
-    $writer.Flush()
 
     $writer.Close()
     $stream.Close()
     $client.Close()
 }
 
-
-$ipAddress = Read-Host "Enter the client's IP address: "
+$ipAddress = Select-IPAddress
 Send-CodeToClient -IPAddress $ipAddress -Port 1234
